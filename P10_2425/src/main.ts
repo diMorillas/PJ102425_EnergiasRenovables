47cf21eba12a1269bcb37b4cc2bf3ecef075459e
diMorillas <didacmorillas19@gmail.com>
2024-12-19 18:06:26 +0100

    Added FormComponent + images

--- P10_2425/src/main.ts
+++ P10_2425/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { NavegatorComponent } from './app/nav.app.component';
+import { FormComponent } from './app/form.app.component';
 
-bootstrapApplication(NavegatorComponent, appConfig)
+
+bootstrapApplication(FormComponent, appConfig)
   .catch((err) => console.error('Error al inicializar la aplicación:', err));
