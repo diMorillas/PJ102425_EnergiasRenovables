722336454a0b8e214149de570ec52b2a5fdcf240
diMorillas <didacmorillas19@gmail.com>
2025-02-05 20:00:16 +0100

    Installed @angular/material

--- P10_2425/src/main.ts
+++ P10_2425/src/main.ts
@@ -1,9 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { NavegatorComponent } from './app/nav.app.component';
+import { FormComponent } from './app/form.app.component';
+import { FooterComponent } from './app/footer.app.component';
+import { AboutUsComponent } from './app/aboutus.app.component';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+import { CardComponent } from './app/card.component';
+import { importProvidersFrom } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      MatButtonModule,
+      MatToolbarModule
+    ), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()
+  ],
+}).catch((err) => console.error(err));
