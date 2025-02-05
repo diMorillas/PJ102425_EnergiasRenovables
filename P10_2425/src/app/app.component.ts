1e3873df261bca58e3ca61b24b6687ea236e6891
diMorillas <didacmorillas19@gmail.com>
2025-02-05 20:45:30 +0100

    Route module added to nav.app, href changed to route way

--- P10_2425/src/app/app.component.ts
+++ P10_2425/src/app/app.component.ts
@@ -2,16 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { AboutUsComponent } from './aboutus.app.component';
-import { FormComponent } from './form.app.component';
-import { CardComponent } from './card.component';
 import { NavegatorComponent } from './nav.app.component';
 import { FooterComponent } from './footer.app.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavegatorComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavegatorComponent, FooterComponent], 
   template: `
     <app-nav></app-nav>
     <router-outlet></router-outlet>
