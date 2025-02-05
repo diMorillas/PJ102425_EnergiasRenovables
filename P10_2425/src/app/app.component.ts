a882abc5ada721ba7a45e0c00303f3173f1b2e75
diMorillas <didacmorillas19@gmail.com>
2025-02-05 20:07:24 +0100

    Now there are some comments to make easier the component test

--- P10_2425/src/app/app.component.ts
+++ P10_2425/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './footer.app.component';  
 import { NavegatorComponent } from './nav.app.component';  
-import { RouterModule } from '@angular/router';  
+import { RouterModule } from '@angular/router';
+import { CardComponent } from "./card.component";  
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, NavegatorComponent, RouterModule],  
+  imports: [FooterComponent, NavegatorComponent, RouterModule, CardComponent],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- P10_2425/src/app/app.routes.ts
+++ P10_2425/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AboutUsComponent } from './aboutus.app.component';
 import { FormComponent } from './form.app.component';
+import { CardComponent } from './card.component';
 
 export const routes: Routes = [
   { path: '', component: AboutUsComponent },  // Página principal
   { path: 'about-us', component: AboutUsComponent },
   { path: 'form', component: FormComponent },
+  {path: 'card', component: CardComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' } // Ruta 404
 ];
