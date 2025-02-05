577bfd484cf0479a75a75e881fa1fa2d5de9ccec
jogaal2425 <joan2005garcia@gmail.com>
2025-02-05 19:41:01 +0100

    Added Material Design

--- P10_2425/src/app/nav.app.component.ts
+++ P10_2425/src/app/nav.app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-navegator',
+  selector: 'app-nav',
   standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, NgIf],
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css'], 
+  styleUrls: ['./nav.component.css']
 })
 export class NavegatorComponent {
-  title = 'navegator'; 
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
+
