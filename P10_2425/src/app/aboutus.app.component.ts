c856f55818cb6140a77a624441a35d4b369c1376
jogaal2425 <joan2005garcia@gmail.com>
2025-02-06 16:55:49 +0100

    Applied Material Design in Form Component  and About Us Component

--- P10_2425/src/app/aboutus.app.component.ts
+++ P10_2425/src/app/aboutus.app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-aboutus',
   standalone: true,
+  imports: [CommonModule, MatButtonModule, RouterModule],
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.css'], 
 })
--- P10_2425/src/app/form.app.component.ts
+++ P10_2425/src/app/form.app.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css'], 
+  styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-  title = 'app-form'; 
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    subject: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required])
+  });
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Formulario enviado:', this.contactForm.value);
+    }
+  }
 }
