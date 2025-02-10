63b275f0ec13f98fb23fc993d17572039353aef9
rekloyd <paumorillasrivera129@gmail.com>
2025-02-10 18:33:49 +0100

    created tablecomponent

---
+++ P10_2425/src/app/table.component.ts
@@ -0,0 +1,42 @@
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Energia } from './models/energia.model';
+import { energies } from './data/energia.data';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginator,MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule
+    
+  ],
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements AfterViewInit {
+  energies: Energia[] = energies;
+  displayedColumns: string[] = ['titulo', 'subtitulo', 'descripcion']; // Define las columnas que necesites
+
+  // Utiliza MatTableDataSource para manejar los datos de la tabla
+  dataSource = new MatTableDataSource<Energia>(this.energies);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+}
