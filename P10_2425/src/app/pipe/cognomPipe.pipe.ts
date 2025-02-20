5bc2d9248079b00c2062b6ddd4d4ded0103411ee
rekloyd <paumorillasrivera129@gmail.com>
2025-02-20 18:10:00 +0100

    deleted files

--- P10_2425/src/app/pipe/cognomPipe.pipe.ts
+++ P10_2425/src/app/pipe/cognomPipe.pipe.ts
@@ -1,10 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'mayus',  // Asegúrate de que el nombre de la pipe sea 'mayus'
+  name: 'mayus',
   standalone: true
 })
-export class CognomPipe implements PipeTransform {  // Renombrada a 'CognomPipe' por convención
+export class CognomPipe implements PipeTransform {
 
   transform(value: string): string {
     return value ? value.toUpperCase() : value;
